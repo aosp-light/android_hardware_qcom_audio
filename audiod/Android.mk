8eda73b91f80ccb1cb1435d3bb751c8a1d6193b2
paul-xxx <thepaulxxx@gmail.com>
2014-11-29 18:09:23 +0600

    audio: fix build

--- audiod/Android.mk
+++ audiod/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-ifeq ($(strip $(BOARD_AUDIO_VARIANT)),caf)
 include $(CLEAR_VARS)
 include external/stlport/libstlport.mk
 
@@ -16,10 +15,7 @@ LOCAL_SHARED_LIBRARIES := \
 	libmedia \
 	libstlport
 
-LOCAL_ADDITIONAL_DEPENDENCIES += device/sony/hikari/kernel-headers
-
 LOCAL_MODULE:= audiod
 LOCAL_MODULE_TAGS:= debug
 
 include $(BUILD_EXECUTABLE)
-endif
--- hal/Android.mk
+++ hal/Android.mk
@@ -65,19 +65,17 @@ include $(BUILD_SHARED_LIBRARY)
 ifeq ($(strip $(TARGET_BOOTLOADER_BOARD_NAME)),fuji)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := audio_effects.conf
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
 LOCAL_MODULE := mixer_paths.xml
+
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
 LOCAL_MODULE_TAGS := optional
+
 LOCAL_MODULE_CLASS := ETC
+
 LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
+
 include $(BUILD_PREBUILT)
 
 endif
